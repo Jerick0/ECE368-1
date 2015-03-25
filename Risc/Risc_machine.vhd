f017292b0b74d92d1f682033255d00462844b456
Chris Camara <ccamara@umassd.edu>
2015-03-24 22:24:20 -0400

    update

--- Risc/Risc_machine.vhd
+++ Risc/Risc_machine.vhd
@@ -52,16 +52,20 @@ signal D_MUX_SEL : STD_LOGIC;
 signal WB_DATA_MUX : STD_LOGIC;
 signal INST_FETCH_BITCH : STD_LOGIC_VECTOR(15 downto 0);
 signal PC : STD_LOGIC_VECTOR(13 downto 0);
-
+signal RST_LINE : STD_LOGIC;
 
 
 begin
+
+R_U : entity work.system_rest
+	port map(	clk						=> clk,
+					rst						=> rst_line);
 
 C_U : entity work.control_toplayer
 	Port map( instruction_fetch   	=> INST_FETCH_BITCH,
 					PC							=>	PC,
 					CLK						=> CLK,
-					RST						=> RST,
+					RST						=> RST_LINE,
 					E_OPCODE					=> EXEC_OP,
 					E_SW_ENABLE				=> EXEC_SW_EN,
 					O_OPA_MUXSEL			=> CNTLA,
@@ -70,12 +74,12 @@ C_U : entity work.control_toplayer
 					WB_SD_ENABLE			=> WB_ST_DATA_EN,
 					D_IN_MUX_SEL			=> D_MUX_SEL,
 					WB_DATAMUX_SEL			=> WB_DATA_MUX,
-					instruction_WB			=>OPCODE
+					instruction_WB			=> OPCODE
 					);
 					
 DP_U : entity work.datapath
 		POrt Map ( CLK 					=> CLK,
-						RST					=> RST,
+						RST					=> RST_LINE,
 						D_IM_MUX_SEL		=> D_MUX_SEL,
 						O_OPA_MUX_SEL		=> CNTLA,
 						O_OPB_MUX_SEL		=> CNTLB,
